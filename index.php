90bb0c6059b4fb0a63bbb7d8ad613013cf93a9fd
ramon1997 <kratosramonsantana@gmail.com>
2023-09-19 08:23:31 -0300

    primeiro prototipo do front

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trabalho em foco</title>
+    <link rel="stylesheet" href="menu.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="barra">
+        <div class="logo">
+            <img src="peso.png">
+            <h1>Trabalho em foco</h1>
+        </div>
+        <nav class="barra">
+            <div class="mobile">
+                <div class="line1"></div>
+                <div class="line2"></div>
+                <div class="line3"></div>
+            </div>
+            <ul class="nav-list">
+                <li><a href="#planos">menu</a></li>
+                <li><a href="#servico">menu</a></li>
+                <li><a href="#" class="login">menu</a></li>
+            </ul>
+        </nav>
+    </div>
+        <div class="banner">
+
+        </div>
+        <!--nessa div meio a gente coloca uma imagem e um texto promovendo nosso proprio site e no futuro a gente vende esse espaço tambem, com mais espaço e mais destaue-->
+        <div class="meio">
+
+        </div>
+        <div class="cards">
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+        </div>
+    <footer>
+        <p>devs: Antony & Ramon</p>
+    </footer>
+    <script src="js.js"></script>
+</body>
+
+</html>
