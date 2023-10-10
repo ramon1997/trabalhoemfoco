b5a7e3b3c1abd30bbaa5c2780e0472dba1d3c41f
ramon1997 <kratosramonsantana@gmail.com>
2023-10-09 23:57:19 -0300

    pagina de perfil

---
+++ perfil.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Seu Perfil - Trabalho em Foco</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/menu.css">
+    <link rel="stylesheet" href="css/perfil.css">
+    <link rel="shortcut icon" href="imagens/tools.png" type="image/x-icon">
+</head>
+<body>
+    <div class="barra">
+        <div class="logo">
+            <img src="imagens/tools.png">
+            <h1>Trabalho em foco</h1>
+        </div>
+        <nav class="barra">
+            <div class="mobile">
+                <div class="line1"></div>
+                <div class="line2"></div>
+                <div class="line3"></div>
+            </div>
+            <ul class="nav-list">
+                <li><a href="index.php">Inicio</a></li>
+                <li><a href="vagas.php">Vagas</a></li>
+                <li><a href="#contatos">Contatos</a></li>
+                <li><a href="#quemsomos">Quem Somos</a></li>
+                <li><a href="perfil.php" class="perfil">Editar seu perfil</a></li>
+            </ul>
+        </nav>
+    </div>
+    <div class="banner">
+        <img src="imagens/arvore.jpg">
+    </div>
+    <div class="foto">
+        <img src="imagens/avatar.png">
+    </div>
+    <div class="informacoes">
+        <h2>Nome completo</h2>
+        <h3>24 Anos</h3>
+        <h3>Secretaria</h3>
+        <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi vel natus excepturi aliquam maxime! Modi animi totam iste eligendi quaerat labore ea consectetur porro, nihil repudiandae sint ex. Deserunt, quisquam?</h3>
+        <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium tempore at, ab sunt rerum illum sapiente esse, enim voluptatibus molestias exercitationem iusto commodi minima impedit, aliquam fuga iure tempora voluptas.</h3>
+    </div>
+    <footer>
+        <p>Copyright © 2023 Antony Dias & Ramon Santana. All right reserved</p>
+    </footer>
+    <script src="javascript/js.js"></script>
+</body>
+</html>
