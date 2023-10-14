047bd4197341ca70d2e3007d5e16b2fa45f24b12
ramon1997 <kratosramonsantana@gmail.com>
2023-10-13 23:35:43 -0300

    add tela de login

--- index.php
+++ index.php
@@ -5,74 +5,43 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Trabalho em foco</title>
-    <link rel="stylesheet" href="css/menu.css">
+    <link rel="stylesheet" href="css/login.css">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/menu.css">
     <link rel="shortcut icon" href="imagens/tools.png" type="image/x-icon">
-
 </head>
 
 <body>
-    <div class="barra">
-        <div class="logo">
-            <img src="imagens/tools.png">
-            <div id="texto"></div>
-        </div>
-        <nav class="barra">
-            <div class="mobile">
-                <div class="line1"></div>
-                <div class="line2"></div>
-                <div class="line3"></div>
-            </div>
-            <ul class="nav-list">
-                <li><a href="vagas.php">Vagas</a></li>
-                <li><a href="#contatos">Contatos</a></li>
-                <li><a href="#quemsomos">Quem Somos</a></li>
-                <li><a href="perfil.php" class="perfil">Seu Perfil</a></li>
-
-            </ul>
-        </nav>
-    </div>
-    <div class="banner">
-        <div class="opaca">
-            <h1>"Nenhum de nós é tão inteligente quanto todos nós juntos." - Ken Blanchard</h1>
-        </div>
+    <div class="quasemenu">
+        <div id="texto"></div>
     </div>
     <div class="titulo">
-        <h1>Contatos</h1>
+        <h1>Login</h1>
     </div>
-    <div class="contatos">
-        <div class="card">
-            <img src="imagens/email.png">
-            <h2>trabalhoemfoco@gmail.com</h2>
+    <div class="container">
+        <div class="texto">
+            <h1>Bem-vindo de volta</h1>
+            <h2>"A jornada de mil milhas começa com um único passo." - Lao Tsé</h2>
+            <hr>
+            <h3>Não tem uma conta? clique no botão abaixo</h3>
+            <nav>
+                <a href="redi.html">Crie uma conta</a>
+            </nav>
         </div>
-        <div class="card">
-            <img src="imagens/insta.png">
-            <h2>@trabalhoemfoco</h2>
+        <div class="entrar">
+            <h3>Preencha os campos abaixo</h3>
+            <form action="" method="post">
+                <label for="email">E-mail:</label>
+                <input type="email" name="email" placeholder="Seu e-mail">
+                <label for="senha">Senha:</label>
+                <input type="password" name="senha" placeholder="Sua senha">
+                <input type="submit" value="Entrar" id="botao">
+            </form>
         </div>
-        <div class="card">
-            <img src="imagens/zap.png">
-            <h2>(75)99999999</h2>
-        </div>
-    </div>
-    <div class="quem">
-        <h1>Quem somos</h1>
-        <p>"Trabalho em foco o seu parceiro confiável na busca por oportunidades de carreira e
-            no recrutamento de talentos. Nossa plataforma foi projetada para simplificar o processo de encontrar
-            empregos ou candidatos ideais. Com uma vasta gama de ofertas de emprego e uma base de dados robusta de
-            currículos, conectamos candidatos talentosos a empresas de renome em todo o país.
-
-            Para os candidatos, oferecemos uma experiência fácil de usar, com pesquisa intuitiva, notificações
-            personalizadas e ferramentas para criar perfis atraentes. Para os empregadores, fornecemos um espaço para
-            publicar vagas, analisar currículos e gerenciar o processo de recrutamento de forma eficiente.
-
-            Independentemente de você estar em busca de um novo desafio profissional ou procurando o candidato perfeito
-            para sua empresa, Trabalho em foco é o seu destino de escolha. Junte-se a nós e leve sua carreira
-            ou seu negócio para o próximo nível."</p>
     </div>
     <footer>
         <p>Copyright © 2023 Antony Dias & Ramon Santana. All right reserved</p>
     </footer>
-    <script src="javascript/js.js"></script>
     <script src="javascript/letrasquesomem.js"></script>
 </body>
 
