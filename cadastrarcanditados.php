e6c2ff54c6fb98d647c540a81bc436e762078ab2
ramon1997 <kratosramonsantana@gmail.com>
2023-10-17 18:08:56 -0300

    paginas de cadastro

---
+++ cadastrarcanditados.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trabalho em foco</title>
+    <link rel="shortcut icon" href="imagens/tools.png" type="image/x-icon">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/menu.css">
+    <link rel="stylesheet" href="css/cadastrarcanditados.css">
+    <link rel="stylesheet" href="css/login.css">
+</head>
+
+<body>
+    <div class="quasemenu">
+        <div id="texto"></div>
+    </div>
+    <div class="titulo">
+        <h1>Cadastro de canditados</h1>
+    </div>
+    <div class="campos">
+        <form action="" method="post">
+            <label for="nome">Seu nome completo:</label>
+            <input type="text" name="nome" id="nome" placeholder="Seu nome">
+            <label for="email">Seu e-mail:</label>
+            <input type="email" name="email" id="email" placeholder="Seu e-mail">
+            <label for="senha">Crie uma senha:</label>
+            <input type="password" name="senha" id="senha" placeholder="Senha">
+            <input type="submit" value="Cadastrar" id="botao">
+        </form>
+    </div>
+    <div class="aviso">
+        <h3>Apos fazer login complete seu perfil para ter mais chances.</h3>
+    </div>
+    <footer>
+        <p>Copyright © 2023 Antony Dias & Ramon Santana. All right reserved</p>
+    </footer>
+    <script src="javascript/letrasquesomem.js"></script>
+</body>
+
+</html>
---
+++ cadastrarempresas.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trabalho em foco</title>
+    <link rel="shortcut icon" href="imagens/tools.png" type="image/x-icon">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/menu.css">
+    <link rel="stylesheet" href="css/cadastrarcanditados.css">
+    <link rel="stylesheet" href="css/login.css">
+</head>
+
+<body>
+    <div class="quasemenu">
+        <div id="texto"></div>
+    </div>
+    <div class="titulo">
+        <h1>Cadastro de empresas</h1>
+    </div>
+    <div class="campos">
+        <form action="" method="post">
+            <label for="nome">Nome completo da empresa:</label>
+            <input type="text" name="nome" id="nome" placeholder="Nome da empresa">
+            <label for="cidade">Cidade:</label>
+            <input type="text" name="cidade" id="cidade" placeholder="Cidade onde se encontra a empresa">
+            <label for="email">Seu e-mail:</label>
+            <input type="email" name="email" id="email" placeholder="Seu e-mail">
+            <label for="senha">Crie uma senha:</label>
+            <input type="password" name="senha" id="senha" placeholder="Senha">
+            <input type="submit" value="Cadastrar" id="botao">
+        </form>
+    </div>
+    <div class="aviso">
+        <h3>Apos fazer login complete seu perfil para passar mais confiança.</h3>
+    </div>
+    <footer>
+        <p>Copyright © 2023 Antony Dias & Ramon Santana. All right reserved</p>
+    </footer>
+    <script src="javascript/letrasquesomem.js"></script>
+</body>
+
+</html>
